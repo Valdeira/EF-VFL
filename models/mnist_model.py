b6ccb1219355355f134c293848cac622bb9cd0de
Valdeira <pedromvaldeira@gmail.com>
2024-10-15 15:27:57 -0400

    multi-seed experiments working

--- models/mnist_model.py
+++ models/mnist_model.py
@@ -3,30 +3,39 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as L
 from torchmetrics import Accuracy
-from models.compressors import TopKCompressor, QSGDCompressor
+from models.compressors import EFCompressor, TopKCompressor, QSGDCompressor
 
 
 compressors_d = {"topk": TopKCompressor, "qsgd": QSGDCompressor}
 
 
 class RepresentationModel(nn.Module):
-    def __init__(self, input_size=196, cut_size=16, compressor=None, compression_parameter=None):
+    def __init__(self, input_size=196, cut_size=16, compressor=None, compression_parameter=None, compression_type=None, num_samples=None):
         super().__init__()
         self.fc = nn.Linear(input_size, cut_size)
         self.sigmoid = nn.Sigmoid()
+        self.compressor = compressor
+        self.compression_parameter = compression_parameter
+        self.compression_type = compression_type
         if compressor is None:
             self.compression_layer = None
         else:
-            print(f"compressor: {compressor}")
-            print(f"type(compressor): {type(compressor)}")
             if compression_parameter is None:
                 raise ValueError("compression_parameter must be provided when a compressor is.")
-            self.compression_layer = compressors_d[compressor](compression_parameter)
+            if compression_type is None:
+                raise ValueError("compression_type must be provided when a compressor is.")
+            if compression_type == "direct":
+                self.compression_layer = compressors_d[compressor](compression_parameter)
+            elif compression_type == "ef":
+                self.compression_layer = EFCompressor(compressors_d[compressor](compression_parameter), cut_size, num_samples)
 
-    def forward(self, x, apply_compression=False):
+    def forward(self, x, apply_compression=False, indices=None, epoch=None):
         x = self.sigmoid(self.fc(x))
         if apply_compression and self.compression_layer is not None:
-            x = self.compression_layer(x)
+            if self.compression_type == "ef":
+                x = self.compression_layer(x, indices, epoch)
+            elif self.compression_type == "direct":
+                x = self.compression_layer(x)
         return x
 
 
@@ -41,39 +50,45 @@ class FusionModel(nn.Module):
 
 class ShallowSplitNN(L.LightningModule):
     def __init__(self, input_size=784, cut_size=16, num_classes=10, lr=1.0, momentum=0.0, num_clients=4, 
-                 compressor=None, compression_parameter=None, private_labels=False):
+                 compressor=None, compression_parameter=None, compression_type=None, private_labels=False, num_samples=None):
         super().__init__()
         self.num_clients = num_clients
         self.private_labels = private_labels
         self.automatic_optimization = False
         self.local_input_size = input_size // num_clients
+        self.cut_size = cut_size
 
         self.representation_models = nn.ModuleList([
-            RepresentationModel(self.local_input_size, cut_size, compressor=compressor, compression_parameter=compression_parameter) 
+            RepresentationModel(self.local_input_size, cut_size, compressor=compressor,
+                                compression_parameter=compression_parameter, compression_type=compression_type, num_samples=num_samples) 
             for _ in range(self.num_clients)
         ])
         self.fusion_model = FusionModel(cut_size, num_classes)
 
+        self.bits_per_element = torch.zeros(1).element_size() * 8
+        self.n_bits_per_round_per_client = self._calculate_n_bits(compressor, compression_parameter)
+        self.cumulative_batches = 0
+        
         self.save_hyperparameters()
 
         self.train_accuracy = Accuracy(task="multiclass", num_classes=num_classes)
         self.val_accuracy = Accuracy(task="multiclass", num_classes=num_classes)
         self.test_accuracy = Accuracy(task="multiclass", num_classes=num_classes)
 
-    def forward(self, x, apply_compression=False):
-        representations = [model(self.get_feature_block(x, i), apply_compression=apply_compression) 
-                           for i, model in enumerate(self.representation_models)]
+    def forward(self, x):
+        """forward pass for inference with the SplitNN model (no compression)"""
+        representations = [model(self.get_feature_block(x, i)) for i, model in enumerate(self.representation_models)]
         return self.fusion_model(representations)
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, indices = batch
 
         optimizers = self.optimizers()
         client_optimizers = optimizers[:self.num_clients]
         fusion_optimizer = optimizers[self.num_clients]
-
+        
         compressed_representations = [
-            model(self.get_feature_block(x, i), apply_compression=True) 
+            model(self.get_feature_block(x, i), apply_compression=True, indices=indices, epoch=self.current_epoch) 
             for i, model in enumerate(self.representation_models)
         ]
 
@@ -110,17 +125,33 @@ class ShallowSplitNN(L.LightningModule):
             fusion_optimizer.zero_grad()
 
             total_loss = (sum(client_losses) + fusion_loss) / (len(client_losses) + 1)
+        
+        n_mbytes = (self.n_bits_per_round_per_client * self.cumulative_batches * self.num_clients) / 8e6
 
         self.train_accuracy.update(y_hat, y)
         self.log("train_loss", total_loss, on_epoch=True, prog_bar=True)
         self.log("train_acc", self.train_accuracy, on_epoch=True, prog_bar=True)
+        self.log('communication_cost', n_mbytes, prog_bar=True)
 
         return total_loss
+    
+    def _calculate_n_bits(self, compressor, compression_parameter):
+        if compressor is None:
+            return self.cut_size * self.bits_per_element
+        elif compressor == 'topk':
+            return self.cut_size * self.bits_per_element * compression_parameter
+        elif compressor == 'qsgd':
+            return self.bits_per_element + self.cut_size * (1 + compression_parameter)
+        else:
+            raise ValueError(f"Unknown compressor: {compressor}")
+    
+    def on_train_batch_end(self, outputs, batch, batch_idx):
+        self.cumulative_batches += 1
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, _ = batch
 
-        y_hat = self(x, apply_compression=False)
+        y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
         
         self.val_accuracy.update(y_hat, y)
@@ -129,9 +160,9 @@ class ShallowSplitNN(L.LightningModule):
         return loss
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
+        x, y, _ = batch
 
-        y_hat = self(x, apply_compression=False)
+        y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
 
         self.test_accuracy.update(y_hat, y)
